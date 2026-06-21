fec90f4d0780a904749d3f4541b6935f13d18e47
deepanshu-mahawar <deepanshu.mahawar@kriotek.in>
2026-06-21 18:45:52 +0530

    add changes

--- ass11.c
+++ ass11.c
@@ -1,115 +1,112 @@
-/*
-// print 10 times deepanshu
-#include<stdio.h>
-int main(){
-    int i = 1;
-    while(i<=10){
-        printf("Deepanshu\n");
-        i++;
-    }
-    return 0;
-}
+// Q1 print deepanshu 5 times
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while(i<=5){
+//         printf("Deepanshu\n");
+//         i++;
+//     }
+//     return 0;
+// }
 
-// print 10 natural numbers
-#include<stdio.h>
-int main(){
-    int i = 1;
-    while(i<=10){
-        printf("%d\n", i);
-        i++;
-    }
-    return 0;
-}
+// Q2 print first 10 natural number
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while(i<=10){
+//         printf("%d\n", i);
+//         i++;
+//     }
+//     return 0;
+// }
 
-// print 10 natural numbers in reverse
-#include<stdio.h>
-int main(){
-    int i = 10;
-    while(i>=1){
-        printf("%d\n", i);
-        i--;
-    }
-    return 0;
-}
+// Q3 print first 10 natural numbers in reverse
+// #include<stdio.h>
+// int main(){
+//     int i=10;
+//     while(i>=1){
+//         printf("%d\n", i);
+//         i--;
+//     }
+//     return 0;
+// }
 
-// print 10 odd numbers
-#include<stdio.h>
-int main(){
-    int i = 0;
-    while(i<10){
-        printf("%d\n", 2*i+1);
-        i++;
-    }
-    return 0;
-}
+// Q4 print first 10 odd natural numbers
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while(i<=10){
+//         printf("%d\n", i*2-1);
+//         i++;
+//     }
+//     return 0;
+// }
 
-// print 10 odd numbers in reverse
-#include<stdio.h>
-int main(){
-    int i = 1;
-    while(i<=10){
-        printf("%d\n", 21-2*1);
-        i++;
-    }
-    return 0;
-}
+// Q5 print first 10 odd natural numbers in reverse
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while(i<=10){
+//         printf("%d\n", 21-2*i);  
+//         i++;
+//     }
+//     return 0;
+// }
 
-// print 10 even numbers
-#include<stdio.h>
-int main(){
-    int i = 1;
-    while (i<=10)
-    {
-        printf("%d\n", 2*i);
-        i++;
-    }
-    
-    return 0;
-}
+// Q6 print first 10 even natural numbers
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while (i<=10)
+//     {
+//         printf("%d\n", 2*i);
+//         i++;
+//     }
+//     return 0;
+// }
 
-// print 10 even numbers in reverse
-#include<stdio.h>
-int main(){
-    int i = 1;
-    while (i<=10)
-    {
-        printf("%d\n", 22-2*i);
-        i++;
-    }
-    
-    return 0;
-}
-    
-// print square of first 10 natural numbers
-#include<stdio.h>
-int main(){
-    int i=1;
-    while(i<=10){
-        printf("Square of %d is %d\n", i, i*i);
-        i++;
-    }    
-    return 0;
-}
-// print cubes of first 10 natural numbers
-#include<stdio.h>
-int main(){
-    int i=1;
-    while(i<=10){
-        printf("Cube of %d is %d\n", i, i*i*i);
-        i++;
-    }    
-    return 0;
-}
+// Q7 print first 10 even natural numbers in reverse
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while(i<=10){
+//         printf("%d\n", 22-2*i); 
+//         i++;
+//     }
+//     return 0;
+// }
 
-// print table of 5
-#include<stdio.h>
-int main(){
-    int i=1;
-    while(i<=10){
-        printf("5 * %d = %d\n", i, 5*i);
-        i++;
-    }    
-    return 0;
-}
-*/
+// Q8 print squares of the first 10 natural numbers
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while (i<=10)
+//     {
+//         printf("%d\n", i*i);
+//         i++;
+//     }
+//     return 0;
+// }
 
+// Q9 print cubes of the first 10 natural numbers
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while (i<=10)
+//     {
+//         printf("%d\n", i*i*i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// Q10 print 5 table
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while (i<=10)
+//     {
+//         printf("5 * %d = %d\n", i, i*5);
+//         i++;
+//     }
+// }
