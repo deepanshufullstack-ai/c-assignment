c6ab53cbb10d416beedee0ae90d207a0c485a84c
deepanshu-mahawar <deepanshu.mahawar@kriotek.in>
2026-06-21 16:10:54 +0530

    add changes

--- ass10.c
+++ ass10.c
@@ -1,69 +1,64 @@
-/*
-#include <stdio.h>
-int main()
-{
-    int n;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-    if (n > 0)
-    {
-        printf("Positive");
-    }
-    else if (n < 0)
-    {
-        printf("Negative");
-    }
-    else
-    {
-        printf("Zero");
-    }
-        return 0;
-}
+// Q1 check whether a given number is positive, negative or zero
+// #include<stdio.h>
+// int main(){
+//     int num;
+//     printf("Enter a number: ");
+//     scanf("%d", &num);
+//     if(num>0){
+//         printf("Positive");
+//     } else if (num<0)
+//     {
+//         printf("Negative");
+//     } else {
+//         printf("Zero");
+//     }
+//     return 0;
+// }
 
-#include <stdio.h>
-int main()
-{
-    char ch;
-    printf("Enter a character:");
-    scanf("%c", &ch);
-    if (ch >= 'A' && ch <= 'Z'){
-        printf("Uppercase");
-    }else if (ch >= 'a' && ch <= 'z')
-    {
-        printf("Lowercase");
-    }else if (ch >= '0' && ch <= '9')
-    {
-        printf("Digit");
-    }else
-    {
-        printf("Special character");
-    }
-    return 0;
-}
+// Q2 check whether a given character is an alphabet, digit or special character
+// #include<stdio.h>
+// int main(){
+//     char ch;
+//     printf("Enter a character: ");
+//     scanf("%c", &ch);
+//     if(ch>='A' && ch<='Z'){
+//         printf("Uppercase alphabet");
+//     } else if (ch>='a' && ch<='z')
+//     {
+//         printf("Lowercase alphabet");
+//     } else if (ch>='0' && ch<='9'){
+//         printf("Digit");
+//     } else {
+//         printf("Special character");
+//     }
+//     return 0;
+// }
 
-#include<stdio.h>
-int main(){
-    int l1, l2, l3;
-    printf("Enter the length of traingle sides:");
-    scanf("%d %d %d", &l1, &l2, &l3);
-    if(l1 == l2 && l2 == l3 && l3 == l1){
-        printf("Valid");
-    } else {
-        printf("Not valid");
-    }
-}
-*/
+// Q3 check whether the triangle is valid or not
+// #include<stdio.h>
+// int main(){
+//     char s1, s2, s3;
+//     printf("Enter side of triangle: ");
+//     scanf("%d%d%d", &s1, &s2, &s3);
+//     if(s1+s2>s3 || s2+s3>s1 || s3+s1>s2 ){
+//         printf("Valid triangle");
+//     } else {
+//         printf("Invalid triangle");
+//     }
+//     return 0;
+// }
 
-#include<stdio.h>
-int main(){
-    int n;
-    printf("Enter the month number: ");
-    scanf("%d", &n);
-    if(n == 1 || n == 3 || n == 5 || n == 7 || n == 8 || n == 11 || n == 12){
-        printf("31 days");
-    } else if(n == 4 || n==6 || n==9 || n==11){
-        printf("30 days");
-    } else {
-        printf("29 days");
-    }
-}
+// Q4 display number of days in that month
+// #include<stdio.h>
+// int main(){
+//     int month;
+//     printf("Enter a month number: ");
+//     scanf("%d", &month);
+//     if(month==1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 9 || month == 10 || month==12){
+//         printf("31 days");
+//     } else if (month == 4 || month == 6 || month == 9 || month == 11){
+//         printf("30 days");
+//     } else {
+//         printf("28 days");
+//     }
+// }
