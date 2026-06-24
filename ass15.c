1db5c7c1d43625c2ce1a27099e8094f9119a4e3e
deepanshu-mahawar <deepanshu.mahawar@kriotek.in>
2026-06-24 09:45:33 +0530

    first commit

--- ass15.c
+++ ass15.c
@@ -93,3 +93,103 @@
 //         }
 //     }
 // }
+
+// Q1 print prime numbers under 100
+// #include<stdio.h>
+// int main(){
+//     int num, isPrime, i;
+//     for(num = 2; num<100; num++){
+//         isPrime=1;
+//         for(i = 2; i<=num/2; i++){
+//             if(num%i==0){
+//                 isPrime = 0;
+//                 break;
+//             }
+//         }
+//         if(isPrime){
+//             printf("%d\n", num);
+//         }
+//     }
+//     return 0;
+// }
+
+// Q2 print prime number between two given numbers
+// #include<stdio.h>
+// int main() {
+//     int n1, n2, num, i;
+//     printf("Enter two numbers: ");
+//     scanf("%d%d", &n1, &n2);
+//     for(num = n1 + 1; num < n2; num++) {
+//         for(i = 2; i < num - 1; i++) {
+//             if(num % i == 0) {
+//                 break;
+//             }
+//         }
+//         if(i == num - 1) {
+//             printf("%d\n", num);
+//         }
+//     }
+//     return 0;
+// }
+
+// Q3 find next prime number of given number
+// #include <stdio.h>
+// int main()
+// {
+//     int num, i, isPrime, j;
+//     printf("Enter a number: ");
+//     scanf("%d", &num);
+//     for (i = num + 1;; i++)
+//     {
+//         isPrime = 1;
+//         for (j = 2; j < i / 2; j++)
+//         {
+//             if (i % j == 0)
+//             {
+//                 isPrime = 0;
+//                 break;
+//             }
+//         }
+//         if (isPrime)
+//         {
+//             printf("%d\n", i);
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+// Q4 calculate HCF of two numbers
+// #include <stdio.h>
+// int main() {
+//     int a, b, temp;
+//     printf("Enter two numbers: ");
+//     scanf("%d %d", &a, &b);
+//     while (b != 0) {
+//         temp = b;
+//         b = a % b;
+//         a = temp;
+//     }
+//     printf("HCF = %d\n", a);
+//     return 0;
+// }
+
+// Q5 whether two given numbers are co-prime or not
+// #include <stdio.h>
+// int main() {
+//     int a, b, temp, gcd;
+//     printf("Enter two numbers: ");
+//     scanf("%d %d", &a, &b);
+//     int x = a, y = b;
+//     while (y != 0) {
+//         temp = y;
+//         y = x % y;
+//         x = temp;
+//     }
+//     gcd = x;
+//     if (gcd == 1)
+//         printf("%d and %d are Co-prime numbers.\n", a, b);
+//     else
+//         printf("%d and %d are not Co-prime numbers.\n", a, b);
+//     return 0;
+// }
--- ass16.c
+++ ass16.c
@@ -60,20 +60,92 @@
 // }
 
 // Q4 check whether a given number is an armstrong number or not
-#include<stdio.h>
-int main(){
-    int num, x, digits;
-    printf("Enter a number: ");
-    scanf("%d", num);
-    x=num;
+// #include<stdio.h>
+// int main(){
+//     int num, x, digits;
+//     printf("Enter a number: ");
+//     scanf("%d", num);
+//     x=num;
 
-    for(digits=0; x; x=x/10)
-    digits++;
-
-
-
-
-}
+//     for(digits=0; x; x=x/10)
+//     digits++;
+// }
 
 // Q5 print all armstrong under 1000
 
+// Q1 find the nth term of fibonacci series
+// #include <stdio.h>
+// int main()
+// {
+//     int n, i;
+//     int a = 0, b = 1, c;
+//     printf("Enter a value of n: ");
+//     scanf("%d", &n);
+
+//     if (n == 1)
+//     {
+//         printf("the %dth fibonacci term is %d\n", n, a);
+//     }
+//     else if (n == 2)
+//     {
+//         printf("the %dth fibonacci term is %d\n", n, a);
+//     }
+//     else
+//     {
+//         for (i = 3; i <= n; i++)
+//         {
+//             c = a + b;
+//             printf("c=%d\n", c);
+//             a = b;
+//             printf("a=%d\n", a);
+//             b = c;
+//             printf("b=%d\n\n", b);
+//         }
+//         printf("the %d is fibonacci number is %d\n", n, b);
+//     }
+//     return 0;
+// }
+
+// Q2 print first n term of fibonacci series
+// #include<stdio.h>
+// int main(){
+//     int n, i;
+//     int a=0, b=1, c;
+//     printf("Enter the number of terms: ");
+//     scanf("%d", &n);
+//     for(i=1; i<=n; i++){
+//         printf("%d\n", a);
+//         c=a+b;
+//         a=b;
+//         b=c;
+//     }
+//     return 0;
+// }
+
+// Q3 given number is in fibonacci series or not
+// #include<stdio.h>
+// int main(){
+//     int n, i;
+//     int a=0, b=1, c;
+//     printf("Enter a number: ");
+//     scanf("%d", &n);
+//     if(n==0 || n==1){
+//         printf("%d is a fibonacci number\n", n);
+//         return 0;
+//     }
+    
+//     while(b<n){
+//         c=a+b;
+//         a=b;
+//         b=c;
+//     }
+
+//     if(b==n){
+//         printf("%d is a fibonacci number", n);
+//     } else {
+//         printf("%d is not a fibonacci number", n);
+//     }
+//     return 0;
+// }
+
+// Q4 check whether the given number is armstrong or not
