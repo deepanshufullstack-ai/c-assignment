8e12aba202a083cb17bd72bafbcd1526b1b1d58d
deepanshu-mahawar <deepanshu.mahawar@kriotek.in>
2026-06-24 10:54:38 +0530

    add codes

--- ass16.c
+++ ass16.c
@@ -1,78 +1,3 @@
-// Q1 find the nth term of the fibonacci series
-// #include<stdio.h>
-// int main(){
-//     int a=-1, b=1, c, n;
-//     printf("Enter a number: ");
-//     scanf("%d", &n);
-//     while (n)
-//     {
-//         c=a+b;
-//         a=b;
-//         b=c;
-//         n--;
-//     }
-//     printf("%d", c);
-//     printf("\n");
-//     return 0;
-// }
-
-// Q2 first n terms of fabonacci series
-// #include<stdio.h>
-// int main(){
-//     int a=-1, b=1, c, n;
-//     printf("Enter a number: ");
-//     scanf("%d", &n);
-//     while (n)
-//     {
-//         c=a+b;
-//         printf("%d", c);
-//         a=b;
-//         b=c;
-//         n--;
-//     }
-//     printf("\n");
-//     return 0;
-// }
-
-// Q3 check whether a given number is there in the fibonacci series or not
-// #include<stdio.h>
-// int main(){
-//     int a=-1, b=1, c, num;
-//     printf("Enter a number: ");
-//     scanf("%d", &num);
-//     while (1)
-//     {
-//         c=a+b;
-//         if(c>=num){
-//             break;
-//         }
-//         a=b;
-//         b=c;
-//     }
-// if(c==num){
-//     printf("%d is in the series", num);
-// }
-// else {
-//     printf("%d is not in the series", num);
-// }
-//     printf("\n");
-//     return 0;
-// }
-
-// Q4 check whether a given number is an armstrong number or not
-// #include<stdio.h>
-// int main(){
-//     int num, x, digits;
-//     printf("Enter a number: ");
-//     scanf("%d", num);
-//     x=num;
-
-//     for(digits=0; x; x=x/10)
-//     digits++;
-// }
-
-// Q5 print all armstrong under 1000
-
 // Q1 find the nth term of fibonacci series
 // #include <stdio.h>
 // int main()
@@ -133,7 +58,7 @@
 //         printf("%d is a fibonacci number\n", n);
 //         return 0;
 //     }
-    
+
 //     while(b<n){
 //         c=a+b;
 //         a=b;
@@ -149,3 +74,44 @@
 // }
 
 // Q4 check whether the given number is armstrong or not
+// #include <stdio.h>
+// int main() {
+//   int num, digit, sum = 0, temp;
+//   printf("Enter a three digit number to find is armstrong or not: ");
+//   scanf("%d", &num);
+//   temp = num;
+//   while (temp > 0) {
+//     digit = temp % 10;
+//     printf("digit=%d\n", digit);
+//     sum = sum + digit * digit * digit;
+//     printf("sum=%d\n", sum);
+//     temp = temp / 10;
+//     printf("num=%d\n\n", temp);
+//   }
+//   if (num == sum) {
+//     printf("%d is armstrong", sum);
+//   } else {
+//     printf("%d is not armstrong", sum);
+//   }
+//   return 0;
+// }
+
+// Q5 print all armstrong under 1000
+// #include<stdio.h>
+// int main(){
+//     int num, temp, sum, digit;
+//     for(num=1; num<=1000; num++){
+//         temp=num;
+//         sum=0;
+//         while(temp>0){
+//             digit=temp%10;
+//             sum=sum+digit*digit*digit;
+//             temp=temp/10;
+//         }
+
+//         if(sum==num){
+//             printf("%d ", num);
+//         }
+//     }
+//     return 0;
+// }
