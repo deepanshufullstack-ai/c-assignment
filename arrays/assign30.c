f5c4913544b3a55e1c5b35fd228ccb5034b87f1c
deepanshu-mahawar <deepanshu.mahawar@kriotek.in>
2026-06-30 11:45:45 +0530

    add codes

---
+++ arrays/assign30.c
@@ -0,0 +1,48 @@
+// #include<stdio.h>
+// int main(){
+//     int a[5], i, j, temp;
+//     printf("Enter 5 numbers: ");
+//     for(i=0; i<5; i++){
+//     scanf("%d", &a[i]);
+//     }
+    
+//     for(i=0; i<5; i++){
+//         for(j=i+1; j<5; j++){
+//             if(a[i]>a[j]){
+//                 temp=a[i];
+//                 a[i]=a[j];
+//                 a[j]=temp;
+//             }
+//         }
+//     }
+    
+//     for(i=0; i<5; i++){
+//         printf("%d ", a[i]);
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int a[5], i, j, temp;
+//     printf("Enter 5 numbers: ");
+//     for(i=0; i<5; i++){
+//     scanf("%d", &a[i]);
+//     }
+    
+//     for(i=0; i<5; i++){
+//         for(j=i+1; j<5; j++){
+//             if(a[i]>a[j]){
+//                 temp=a[i];
+//                 a[i]=a[j];
+//                 a[j]=temp;
+//             }
+//         }
+//     }
+    
+//     for(i=0; i<5; i++){
+//         printf("%d\n", a[i]);
+//     }
+//     printf("Largest element = %d\n", a[4]);
+//         printf("Second largest element = %d\n", a[3]);
+    
+// }
